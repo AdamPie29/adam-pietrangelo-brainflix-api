4928e53b202cd7569c852646990829816bff9b73
Adam Pietrangelo <apietrangelo44@gmail.com>
2022-11-16 15:57:40 -0500

    adds proper video json file

--- index.js
+++ index.js
@@ -3,15 +3,17 @@ const express = require('express');
 
 const app = require('express')();
 
+
 app.use(express.static('public'));
+app.use(express.json());
 
 app.use((request, response, next)=> {
     next();
 });
 
-const commmonRoutes = require('./routes/videos');
+const videosRoutes = require('./routes/videos');
 
-app.use("/videos", commmonRoutes);
+app.use("/videos", videosRoutes);
 
 app.listen(8080, ()=> {
     console.log("listening on 8080")
--- routes/videos.js
+++ routes/videos.js
@@ -4,6 +4,11 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/videos", (request , response)=> {
-    response.send()
+    response.send(["test"])
 });
 
+router.get("./videos/:id", (request , response)=> {
+    response.send(["testing"])
+})
+
+module.exports = router;
